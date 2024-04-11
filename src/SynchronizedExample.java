45ed3f97462cdcc0380fa477d09e1c66b357bf46
MrAlamb <aambrazhej12@gmail.com>
2024-04-11 12:32:39 +0300

    FINAL!

--- src/SynchronizedExample.java
+++ src/SynchronizedExample.java
@@ -1,7 +1,6 @@
 public class SynchronizedExample extends Thread {
     private int count = 0;
 
-    Thread thisThread = new Thread(this);
 
     public synchronized void increment() {
         count++;
